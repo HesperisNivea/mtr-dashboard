97512c07c7fc087b9413a9dcb9d047a1411754a0
HesperisNivea <143076071+HesperisNivea@users.noreply.github.com>
2025-09-02 21:31:32 +0200

    fix: gitignore

--- src/types/agenda.d.ts
+++ src/types/agenda.d.ts
@@ -1,14 +1,14 @@
 export type AgendaEvent = {
-	id: string;
-	subject: string;
-	emailAddress: string;
-	location: {
-		displayName: string;
-	};
-	start: {
-		dateTime: string;
-	};
-	end: {
-		dateTime: string;
-	};
+    id: string;
+    emailAddress: string;
+    subject: string;
+    location: {
+        displayName: string;
+    };
+    start: {
+        dateTime: string;
+    };
+    end: {
+        dateTime: string;
+    };
 };
